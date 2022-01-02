5fd5d4f65abbb77449dc59801fdd5669dd969ad2
John Royceton <roycetonis@gmail.com>
2022-01-02 09:46:49 -0500

    fix: removed logo reference

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import logo from './logo.svg';
 import './App.css';
 import Sample from './Sample';
 
@@ -9,20 +8,6 @@ function App() {
 
   return (
     <div className="App">
-      { /* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */ }
       <h1 onClick={ () => { setVal("Aaron") } } > Click on Me </h1>
       <Sample name={val} />
     </div>
